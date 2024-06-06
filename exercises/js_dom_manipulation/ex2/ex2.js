93ff5de1d3c6645f2a559aeb7b21ac3609885d48
Leon Qian <66791776+leon-qian@users.noreply.github.com>
2024-06-07 03:32:00 +1000

    Do js_dom_manipulation ex2

--- exercises/js_dom_manipulation/ex2/ex2.js
+++ exercises/js_dom_manipulation/ex2/ex2.js
@@ -1,3 +1,12 @@
-console.log("Exercise 2")
+console.log("Exercise 2");
 
-const main = document.querySelector("#display-box")
+const main = document.querySelector("#display-box");
+
+const texts = Array(3)
+    .fill(1)
+    .map((x, i) => x + i)
+    .map(n => document.getElementById(`text${n}`));
+
+const changeToText = (n => {
+    main.innerHTML = texts[n - 1].innerHTML;
+});
