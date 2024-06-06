85f3bb8f1463bbd6000093661bb0f611791b12e6
Leon Qian <66791776+leon-qian@users.noreply.github.com>
2024-06-06 18:19:29 +1000

    Do js_dom_manipulation ex1

--- exercises/js_dom_manipulation/ex1/ex1.js
+++ exercises/js_dom_manipulation/ex1/ex1.js
@@ -1,4 +1,19 @@
-console.log("Yay! The javascript is running")
+"use strict";
+console.log("Yay! The javascript is running");
 
-// Cat picture source 
-// https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg
+// Set my name in the element with id `name`.
+document.getElementById("name").innerText = "Leon";
+
+// Make the text bigger and green.
+const bigger = document.getElementById("bigger");
+bigger.style.fontSize = "x-large";
+bigger.style.color = "#00ff00";
+
+// Replace image with cat.
+document.querySelector("img").src = "https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1-768x512.jpg";
+
+// Make all squares visible.
+const squares = document.getElementsByClassName("square");
+for (const square of squares) {
+    square.classList.remove("hidden");
+}
